49ceb36861c27bf2ca0edffc8367947406063b7f
XoXo-GG <152623407+XoXo-GG@users.noreply.github.com>
2023-12-01 18:59:07 +0200

    Update AllInOne_test.js

--- AllInOne_test.js
+++ AllInOne_test.js
@@ -5,7 +5,10 @@
     function loadScript(repository, file) {
         const scriptURL = `https://raw.githubusercontent.com/${repository}/main/${file}`;
 
-        GM_xmlhttpRequest({
+        const script2 = document.createElement('script');
+        script2.src = scriptURL;
+        document.head.appendChild(script2);
+        /*GM_xmlhttpRequest({
             method: 'GET',
             url: scriptURL,
             onload: function(response) {
@@ -14,8 +17,15 @@
                 scriptElement.textContent = scriptCode;
                 document.head.appendChild(scriptElement);
             }
-        });
+        });*/
     }
+
+// script1.js
+console.log('Script 1 executed');
+
+// Dynamically load script2.js
+
+
     if(window.location.href.indexOf("screen=main") > -1){
         loadScript(repository,autoBuilder);
     }
